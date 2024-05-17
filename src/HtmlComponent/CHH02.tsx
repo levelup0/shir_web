1d40205a727359cb8999a8e26c31a705e9b90cb1
Lev Rybakov <leo.rybakov@icloud.com>
2024-05-18 01:53:46 +0300

    delete unused HtmlComponents

--- src/HtmlComponent/CHH02.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default function CHH02({ title }: any) {
-    return (
-        <h2 className="text-black text-[32px] font-semibold leading-[32px]">
-            {title}
-        </h2>
-    );
-}
